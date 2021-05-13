a5c460be0013daa55e84d0cf6bfe8f98057d7409
fusimeng <fufaloveguomengkai@163.com>
2021-05-13 08:59:09 +0800

    fixed 无return

--- models/densenet.py
+++ models/densenet.py
@@ -11,7 +11,7 @@ from torchvision.models.densenet import densenet201
 
 
 class Densenet121(torch.nn.Module):
-    def __init__(self, num_class=1000, in_channels=3, pretrained=False):
+    def __init__(self, num_class=1000, in_channels=3, pretrained=False, **kwargs):
         super(Densenet121, self).__init__()
         self.model = densenet121(pretrained=pretrained)
         self.model.features[0] = torch.nn.Conv2d(in_channels=in_channels, out_channels=64, kernel_size=7,
@@ -19,4 +19,4 @@ class Densenet121(torch.nn.Module):
         self.model.classifier = torch.nn.Linear(in_features=1024, out_features=num_class, bias=True)
 
     def forward(self, x):
-        self.model(x)
+        return self.model(x)
