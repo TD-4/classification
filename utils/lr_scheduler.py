18f4e8c83f20ade29f048a4a16f3c3d83599a765
fusimeng <fufaloveguomengkai@163.com>
2021-05-17 14:43:18 +0800

    add poly ConsineAnnealingLR_

--- utils/lr_scheduler.py
+++ utils/lr_scheduler.py
@@ -1,10 +1,36 @@
 import math
 from torch.optim.lr_scheduler import StepLR
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from torch.optim.lr_scheduler import _LRScheduler
 
 
 class StepLR_(StepLR):
     def __init__(self, optimizer, step_size=1, gamma=0.5):
         super(StepLR_, self).__init__(optimizer, step_size, gamma)
+
+
+class Poly(_LRScheduler):
+    def __init__(self, optimizer, num_epochs, iters_per_epoch=0, warmup_epochs=0, last_epoch=-1):
+        self.iters_per_epoch = iters_per_epoch
+        self.cur_iter = 0
+        self.N = num_epochs * iters_per_epoch
+        self.warmup_iters = warmup_epochs * iters_per_epoch
+        super(Poly, self).__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        T = self.last_epoch * self.iters_per_epoch + self.cur_iter
+        factor = pow((1 - 1.0 * T / self.N), 0.9)
+        if self.warmup_iters > 0 and T < self.warmup_iters:
+            factor = 1.0 * T / self.warmup_iters
+
+        self.cur_iter %= self.iters_per_epoch
+        self.cur_iter += 1
+        return [base_lr * factor for base_lr in self.base_lrs]
+
+
+class ConsineAnnealingLR_(CosineAnnealingLR):
+    def __init__(self, optimizer, step_size=1, gamma=0.5):
+        super(ConsineAnnealingLR_, self).__init__(optimizer, T_max=10, eta_min=0)
 
 
 if __name__ == "__main__":
@@ -24,8 +50,8 @@ if __name__ == "__main__":
     iters_per_epoch = 10
     lrs = []
     mementums = []
-    lr_scheduler = StepLR_(optimizer, 10, 0.1)
-
+    # lr_scheduler = StepLR_(optimizer, 10, 0.1)
+    lr_scheduler = ConsineAnnealingLR_(optimizer, 10, 0)
     for epoch in range(epochs):
         for i in range(iters_per_epoch):
             pass
